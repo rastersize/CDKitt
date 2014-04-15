4366ccb9dfca4c99dee1bc65eadb23f8b28591cb
Aron Cedercrantz <aron@cedercrantz.se>
2014-04-15 15:12:46 +0200

    Fix build issue in Xcode 5.1.
    - For some reason Xcode refused to find one of the functions even though it
    exist and all of its siblings worked fine. Only seemed to be an issue during
    compile time and not runtime so remove `inline` keyword which was the trigger.

--- CDKitt/CDDispatchBlock.h
+++ CDKitt/CDDispatchBlock.h
@@ -13,22 +13,22 @@
 #pragma mark - Dispatching Blocks on Queues
 /** @name Dispatching Blocks on Queues */
 /// Dispatch a block on the main queue synchronously.
-extern inline void CDDispatchBlockOnMainQueue(dispatch_block_t block);
+extern void CDDispatchBlockOnMainQueue(dispatch_block_t block);
 
 /// Dispatch a block on the main queue asynchronously.
-extern inline void CDDispatchBlockOnMainQueueAsync(dispatch_block_t block);
+extern void CDDispatchBlockOnMainQueueAsync(dispatch_block_t block);
 
 /// Dispatch a _block_, unless it is `nil` on the given _queue_ synchronously.
-extern inline void CDDispatchPossibleBlockOnQueue(dispatch_queue_t queue, dispatch_block_t block);
+extern void CDDispatchPossibleBlockOnQueue(dispatch_queue_t queue, dispatch_block_t block);
 
 /// Dispatch a _block_, unless it is `nil` on the given _queue_ asynchronously.
-extern inline void CDDispatchPossibleBlockOnQueueAsync(dispatch_queue_t queue, dispatch_block_t block);
+extern void CDDispatchPossibleBlockOnQueueAsync(dispatch_queue_t queue, dispatch_block_t block);
 
 /// Dispatch a _block_, unless it is `nil` on the main queue synchronously.
-extern inline void CDDispatchPossibleBlockOnMainQueue(dispatch_block_t block);
+extern void CDDispatchPossibleBlockOnMainQueue(dispatch_block_t block);
 
 /// Dispatch a _block_, unless it is `nil` on the main queue asynchronously.
-extern inline void CDDispatchPossibleBlockOnMainQueueAsync(dispatch_block_t block);
+extern void CDDispatchPossibleBlockOnMainQueueAsync(dispatch_block_t block);
 
 
 #pragma mark - Executing a Block in Place
